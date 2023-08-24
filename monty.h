7a0652c8c0ab69ce54f87d3b458cd50066fb668d
Mskithao <patriciakithao@gmail.com>
2023-08-24 21:09:01 +0000

    monty.h edit

--- monty.h
+++ monty.h
@@ -1,8 +1,5 @@
 #ifndef MONTY_H
 #define MONTY_H
-#include <stdlib.h>
-#include <stdio.h>
-
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -20,22 +17,8 @@ typedef struct stack_s
         struct stack_s *next;
 } stack_t;
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
+/* Function prototypes */
+void push(stack_t **stack, unsigned int line_number, int value);
+void pall(stack_t **stack, unsigned int line_number);
 
-
-
-
-
-#endif
+#endif /* MONTY_H */
