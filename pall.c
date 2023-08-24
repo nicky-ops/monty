143b313c87843d92020645a3debbbdee0e32b63a
Mskithao <patriciakithao@gmail.com>
2023-08-24 21:06:44 +0000

    opcode for pall

--- pall.c
+++ pall.c
@@ -1,20 +1,11 @@
+#include <stdio.h>
 #include "monty.h"
-/**
- * pall - displays the stack
- * @stack: current stack
- * @counter: line counter
- * Return: void
- */
 
-void pall(stack_t **stack, unsigned int counter)
-{
-	stack_t *current = *stack;
+void pall(stack_t **stack, unsigned int line_number) {
+    stack_t *current = *stack;
 
-	if (current == NULL)
-		return;
-	while (current)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
+    while (current != NULL) {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
 }
