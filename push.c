0186683d903f0b049c842581a26040cfb7be1cd8
Mskithao <patriciakithao@gmail.com>
2023-08-24 21:04:07 +0000

    push opcode

--- push.c
+++ push.c
@@ -1,30 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "monty.h"
-/**
- * push - push elements into a stack
- * @value: element to be pushed into a stack
- * @stack: pointer to the stack
- * @line_number: line number
- * Return: void
- */
 
-void push(stack_t **stack, int value, unsigned int line_number)
-{
-	if (value)
-	{
-		stack_t *new_node = malloc(sizeof(stack_t));
+void push(stack_t **stack, unsigned int line_number, int value) {
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (!new_node) {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-		if (!new_node)
-		{
-			fprintf(stderr, "Error: malloc failed\n");
-			exit(EXIT_FAILURE);
-		}
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
 
-		new_node->n = value;
-		new_node->prev = NULL;
-		new_node->next = *stack;
+    if (*stack) {
+        (*stack)->prev = new_node;
+    }
 
-		if (*stack)
-			(*stack)->prev = new_node;
-		*stack = new_node;
-	}
+    *stack = new_node;
 }
